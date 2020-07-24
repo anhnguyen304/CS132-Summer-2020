344835a6bc48ecaa69ef554805494de5bd29560f
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-07-23 17:00:28 -0700

    add StringArray folder

---
+++ AnhNguyen/StringArray/StringArray/Program.cs
@@ -0,0 +1,32 @@
+﻿/** Name: Nguyen, Anh
+ * Class: CS132
+ * Project: StringArray
+ * Date: 2020-07-21
+ * Description: a C# Console application project called Types.
+ ***/
+using System;
+
+namespace StringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] food = new string[4];
+            food[0] = "chicken";
+            food[1] = "beef";
+            food[2] = "shrimp";
+            food[3] = "fish";
+            Array.Sort(food);
+            Console.WriteLine("What to eat");
+            Console.WriteLine(food[0] + "," + food[1] + "," + food[2] + "," + food[3]);
+            Console.WriteLine(food.Length);
+
+           
+
+
+
+        }
+    }
+}
