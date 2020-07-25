2ff53dd2224aad68d75f7ec28600395291b318e8
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-07-24 23:59:55 -0700

    HelloWorld

---
+++ AnhNguyen/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            string Name = "Anh";
+            int Age = 27;
+            bool isFemale = true;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+
+            double MyFloat = 10.1;
+            Console.WriteLine(MyFloat);
+
+              
+        }
+    }
+}
