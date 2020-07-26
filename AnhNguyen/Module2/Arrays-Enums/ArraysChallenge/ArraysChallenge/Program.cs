47d23caeb43777ccb0ce88b7ad509d6207691e76
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-07-25 17:31:53 -0700

    adding module 2

---
+++ AnhNguyen/Module2/Arrays-Enums/ArraysChallenge/ArraysChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraysChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentence = { "This", "is", "a", "string" };
+            foreach (string item in sentence)
+
+            Console.Write(item + " ");
+            
+           
+
+        }
+    }
+}
