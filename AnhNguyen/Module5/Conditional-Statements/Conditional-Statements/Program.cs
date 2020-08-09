17837a117bbcd8a7d7e1b9e336bdf457020f488f
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-08-08 23:55:15 -0700

    adding HOP05

---
+++ AnhNguyen/Module5/Conditional-Statements/Conditional-Statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            Console.WriteLine("Guess a number (1 - 10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine());//receive user input and store in "userInput" variable
+
+            if (userInput == num)
+            {
+                Console.WriteLine("Your guess was correct!");
+            }  
+            else if (userInput > 10)
+            {
+                Console.WriteLine("Number must be between 1 - 10");
+            }
+        }
+    }
+}
