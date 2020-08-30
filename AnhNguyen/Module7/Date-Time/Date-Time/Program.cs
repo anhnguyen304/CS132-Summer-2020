59818d67857e8b113a6d32965d68da42bf7deb1e
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-08-30 11:22:52 -0700

    adding HOP 07

---
+++ AnhNguyen/Module7/Date-Time/Date-Time/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        void GetPersonalInfo()
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What year were you born?: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+
+            int age = Convert.ToInt32(DateTime.Now.Year) - birthYear;
+
+            Console.WriteLine("Here are what we know from your inputs: ");
+            Console.WriteLine("Your name: " + name);
+            Console.WriteLine("Today is: " + DateTime.Now.ToShortDateString());
+            Console.WriteLine("Your age: " + age);
+        }
+        static void Main(string[] args)
+        {
+            Program MyProgram = new Program();
+
+            MyProgram.GetPersonalInfo();
+            
+            int numDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(numDays);
+        }
+        
+        
+            
+            
+        
+
+    }
+}
