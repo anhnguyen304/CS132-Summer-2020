9e0e9b354f72624e65f24223e98d94d336ec0aca
A.S.D <nguyentm@Tus-MacBook-Pro.local>
2020-08-30 21:34:19 -0700

    adding HOP 06

---
+++ AnhNguyen/Module6/String-Manipulation/String-Manipulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS132";
+            string message = Welcome + CourseName;
+
+            for (int i = 0; i < message.Length; i++) 
+
+            {
+                if(i%2 == 0)
+                {
+                    Console.Write(message[i]);
+                }
+
+            }
+            
+               
+        }
+    }
+}
